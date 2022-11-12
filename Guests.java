4296c4b6bd1cf4b71dc9ed7eba84fe2932475497
RogueTurtle <66068126+RogueTurtle@users.noreply.github.com>
2022-11-12 18:22:48 +0000

    completed guest.java
    creates guest object for reservation
    also updated reservations.csv

---
+++ Guests.java
@@ -0,0 +1,46 @@
+//creates guest object
+public class Guests {
+    private String name;
+    private String date;
+    private int phoneNum;
+    private int guestNum;
+    private int restId;
+    private String time;
+    private int tableId;
+
+    Guests(String name, String date, int phoneNum, int guestNum, int restId, String time, int tableId) {
+        this.name = name;
+        this.date = date;
+        this.phoneNum = phoneNum;
+        this.guestNum = guestNum;
+        this.restId = restId;
+        this.time = time;
+        this.tableId = tableId;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getDate() {
+        return date;
+    }
+    public String getTime() {
+        return time;
+    }
+    public int getTableId() {
+        return tableId;
+    }
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+    public int getGuestNum() {
+        return guestNum;
+    }
+    public int getRestId() {
+        return restId;
+    }
+    public String toString(){
+        return (getRestId()+ ","+ getTableId() + ","+ getName()+ "," + getGuestNum() + "," + getDate() + "," + getTime());
+    }
+}
